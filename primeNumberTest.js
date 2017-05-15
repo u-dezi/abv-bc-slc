d578e9dd4d82beee252377ba8b7da02d93242056
Udeme Eno-Nta <denenta@gmail.com>
2017-05-15 21:01:01 +0100

    Add files via upload
    Add new test cases

---
+++ primeNumberTest.js
@@ -0,0 +1,22 @@
+describe('toEqual', function() {
+    // test case 1
+	it('passes if computes prime of 3', function() {
+		expect(findPrime(3)).toEqual([2, 3]);
+    });
+	// test case 2
+	it('passes if computes prime of 5', function() {
+		expect(findPrime(5)).toEqual([2, 3, 5]);
+    });
+	 //test case 3
+	it('passes if computes prime of 2', function() {
+		expect(findPrime(2)).toEqual([2]);
+    });
+	 //test case 4
+	it('passes if showes error message for string', function() {
+		expect(findPrime('just messing with you')).toEqual("Please enter a number");
+    });
+	//test case 5
+	it('passes if showes 1 is not prime', function() {
+		expect(findPrime(1)).toEqual("Not a prime number");
+    });
+});
